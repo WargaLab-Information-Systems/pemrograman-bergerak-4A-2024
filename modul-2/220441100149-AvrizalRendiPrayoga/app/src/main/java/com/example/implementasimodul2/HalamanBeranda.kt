86cf84a37c70303ea4ad53f27ef02bcce2e5ba18
avrzll <220441100149@student.trunojoyo.ac.id>
2024-04-05 10:10:48 +0700

    220441100149 - Avrizal Rendi Prayoga - Modul 2 - Kak Nuri

---
+++ modul-2/220441100149-AvrizalRendiPrayoga/app/src/main/java/com/example/implementasimodul2/HalamanBeranda.kt
@@ -0,0 +1,11 @@
+package com.example.implementasimodul2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanBeranda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_beranda)
+    }
+}
