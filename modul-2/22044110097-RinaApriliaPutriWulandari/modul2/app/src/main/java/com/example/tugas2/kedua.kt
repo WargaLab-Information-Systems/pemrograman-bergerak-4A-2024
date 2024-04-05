849e59395d220337e41139a592464a40c375cb10
riliapuw <rinaapril1504@gmail.com>
2024-04-05 12:58:36 +0700

    220441100097-Rina Aprilia Putri wulandari-modul 2-Kak Nuri

---
+++ modul-2/22044110097-RinaApriliaPutriWulandari/modul2/app/src/main/java/com/example/tugas2/kedua.kt
@@ -0,0 +1,26 @@
+package com.example.tugas2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val Imageview = findViewById<ImageView>(R.id.image)
+        Imageview.setOnClickListener {
+            val nextPage = Intent(this, MainActivity3::class.java)
+            startActivity(nextPage)
+        }
+    }
+}
