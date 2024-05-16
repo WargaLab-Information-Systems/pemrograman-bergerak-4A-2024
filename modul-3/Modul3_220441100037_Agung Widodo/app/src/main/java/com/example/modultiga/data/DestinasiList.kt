52089c7d6ab74f2b60ece079e39ba03aca39806a
agungwidodo037 <agngwidodooo@gmail.com>
2024-05-16 18:57:55 +0700

    220441100037-AgungWidodo-Modul3-KakAngga

---
+++ modul-3/Modul3_220441100037_Agung Widodo/app/src/main/java/com/example/modultiga/data/DestinasiList.kt
@@ -0,0 +1,91 @@
+package com.example.modultiga.data
+
+import com.example.modultiga.R
+
+object DestinasiList {
+    val LokasiDum = arrayListOf(
+        LocationData(
+            "Kawah Ijen",
+            "Kawah Ijen adalah sebuah kawah vulkanik yang terletak di Taman Nasional Ijen, di ujung timur Jawa, Indonesia. Kawah ini terkenal karena keunikan danau kawahnya yang berwarna biru kehijauan, yang merupakan salah satu danau kawah terbesar di dunia. Salah satu fitur paling mencolok dari Kawah Ijen adalah kemunculan api biru yang terlihat di malam hari, yang sebenarnya adalah gas belerang yang terbakar secara alami saat kontak dengan udara.",
+            "Banyuwangi",
+            R.drawable.ijen
+        ),
+        LocationData(
+            "Gili Ketapang",
+            "Gili Ketapang adalah sebuah pulau kecil yang terletak di lepas pantai Probolinggo, Jawa Timur, Indonesia. Pulau ini menawarkan pesona alam yang menakjubkan dengan pantai pasir putih dan air laut yang jernih. Gili Ketapang terkenal sebagai destinasi wisata yang tenang dan cocok untuk bersantai sambil menikmati keindahan alam bawah laut yang kaya akan terumbu karang dan kehidupan laut. Pengunjung dapat melakukan kegiatan seperti snorkeling, menyelam, atau sekadar bersantai di pinggir pantai sambil menikmati pemandangan sunset yang memukau.",
+            "Probolinggo",
+            R.drawable.ketapang
+        ),
+        LocationData(
+            "Pantai Pandawa",
+            "Pantai Pandawa menawarkan suasana yang tenang dan pemandangan yang memukau, menjadikannya tujuan yang populer bagi para wisatawan yang ingin menghindari keramaian pantai-pantai utama di Bali. Aktivitas yang populer di sini termasuk berjemur di pantai, berenang, atau sekadar menikmati pemandangan indah sepanjang pantai.",
+            "Bali",
+            R.drawable.pandawa
+        ),
+        LocationData(
+            "Bromo",
+            "Gunung Bromo memiliki ketinggian sekitar 2.329 meter di atas permukaan laut dan terletak di tengah kaldera sekitar 10 kilometer lebar. Kawah gunung ini terkenal dengan ukuran yang relatif kecil, tetapi pemandangan sekitarnya yang dilengkapi dengan lautan pasir, puncak gunung berapi lainnya seperti Gunung Batok dan Gunung Semeru yang menjulang di kejauhan, membuatnya menjadi tempat yang sangat fotogenik.",
+            "Malang",
+            R.drawable.bromo
+        )
+    )
+
+    val HotelDum = arrayListOf(
+        HotelData(
+            "King Hotel",
+            48,
+            4.9,
+            "King Hotel, terletak di jantung kota, adalah hotel bintang empat yang menawarkan kenyamanan dan kemewahan dengan harga kompetitif. Dengan 150 kamar yang luas dan modern, hotel ini dilengkapi dengan fasilitas seperti AC, Wi-Fi gratis, dan minibar. Para tamu dapat menikmati sarapan prasmanan di restoran yang menyajikan masakan lokal dan internasional, bersantai di kolam renang outdoor dengan pemandangan kota, atau berolahraga di pusat kebugaran.",
+            R.drawable.king
+        ),
+        HotelData(
+            "Rose",
+            34,
+            4.7,
+            "Hotel Rose terletak di jantung kota, hanya beberapa langkah dari pusat perbelanjaan, restoran, dan atraksi budaya utama. Lokasinya yang strategis menawarkan akses mudah ke transportasi umum dan hanya 30 menit berkendara dari bandara internasional.",
+            R.drawable.rose
+        ),
+        HotelData(
+            "Melati",
+            27,
+            4.7,
+            "Hotel Melati adalah tempat menginap eksklusif yang terletak di lokasi yang strategis di pusat kota. Dengan 150 kamar yang elegan dan dilengkapi dengan fasilitas modern seperti AC, TV layar datar, minibar, dan Wi-Fi gratis, Hotel Melati menawarkan kenyamanan tingkat tinggi bagi para tamu. Restoran di hotel menyajikan hidangan lezat dari masakan lokal dan internasional, sementara bar yang chic merupakan tempat yang sempurna untuk bersantai dengan minuman favorit. Fasilitas rekreasi termasuk kolam renang indoor untuk bersantai, spa untuk relaksasi total, dan pusat kebugaran yang dilengkapi dengan peralatan terbaik.",
+            R.drawable.melati
+        ),
+        HotelData(
+            "Tulip",
+            26,
+            4.8,
+            "Hotel Tulip adalah destinasi menginap yang menawan di pusat kota, menawarkan pengalaman yang menggabungkan kenyamanan modern dengan sentuhan kemewahan klasik. Dengan 180 kamar yang elegan dan dilengkapi dengan fasilitas tinggi seperti AC, TV layar datar, brankas, dan Wi-Fi gratis, Hotel Tulip menghadirkan kenyamanan terbaik bagi para tamu. Restoran di hotel menyajikan hidangan lezat dari berbagai masakan internasional dan lokal, sementara lounge dan bar yang chic merupakan tempat yang sempurna untuk bersantai dengan minuman favorit.",
+            R.drawable.tulip
+        ),
+        HotelData(
+            "Kusuma",
+            29,
+            4.5,
+            "Hotel Kusuma adalah destinasi penginapan yang elegan dan nyaman, berlokasi di area yang strategis di pusat kota. Menawarkan 150 kamar yang dirancang dengan gaya modern dan dilengkapi dengan fasilitas unggulan seperti AC, TV layar datar, minibar, dan akses Wi-Fi gratis, Hotel Kusuma memastikan kenyamanan maksimal bagi para tamu. Restoran hotel menyajikan beragam masakan lokal dan internasional, sementara lounge yang nyaman menjadi tempat ideal untuk bersantai dengan minuman favorit.",
+            R.drawable.kusuma
+        ),
+        HotelData(
+            "Dewata",
+            30,
+            4.3,
+            "Hotel Dewata adalah hotel bintang lima yang menawarkan kemewahan dan kenyamanan di lokasi yang strategis di pusat kota. Dengan 200 kamar yang elegan dan dilengkapi dengan fasilitas modern seperti AC, TV layar datar, minibar, dan akses Wi-Fi gratis, Hotel Dewata memberikan pengalaman menginap yang tak terlupakan. Restoran hotel menyajikan berbagai hidangan gourmet lokal dan internasional, sementara bar eksklusif menyediakan tempat sempurna untuk menikmati koktail.",
+            R.drawable.dewata
+        ),
+        HotelData(
+            "Kokoon",
+            29,
+            4.5,
+            "Hotel Kokoon adalah tempat menginap yang unik dan berkelas di pusat kota, menggabungkan desain kontemporer dengan sentuhan keindahan alam. Dengan 100 kamar yang didesain dengan gaya modern dan dilengkapi dengan fasilitas seperti AC, TV layar datar, akses Wi-Fi gratis, dan area duduk yang nyaman, Hotel Kokoon menawarkan pengalaman menginap yang santai namun mewah. Para tamu dapat menikmati hidangan lezat di restoran hotel yang menyajikan masakan kreatif dengan bahan-bahan lokal berkualitas tinggi.",
+            R.drawable.kokoon
+        ),
+        HotelData(
+            "Delima",
+            30,
+            4.3,
+            "Hotel Delima adalah pilihan sempurna untuk pelancong yang mencari kenyamanan dan keanggunan di pusat kota. Hotel bintang empat ini memiliki 180 kamar yang luas dan modern, dilengkapi dengan fasilitas unggulan seperti AC, TV layar datar, minibar, dan akses Wi-Fi gratis. Restoran di Hotel Delima menawarkan beragam hidangan lezat dari masakan lokal dan internasional, sementara bar yang stylish adalah tempat ideal untuk bersantai dengan minuman pilihan.",
+            R.drawable.delima
+        )
+    )
+}
