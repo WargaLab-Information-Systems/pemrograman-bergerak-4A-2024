9265618f2e37bd7256acabcf9f24c27dda47b282
shafirazkh <shafirafira1611@gmail.com>
2024-05-16 19:09:05 +0700

    220441100060-SHAFIRA ZUKHRUFATUZ ZAHRA-FANKY ABDILQOYYIM-MODUL 3

---
+++ modul-3/220441100060_ShafiraZukhrufatuzZahra/Praktikum03/app/src/main/java/com/example/praktikum03/data/HotelDataList.kt
@@ -0,0 +1,106 @@
+package com.example.praktikum03.data
+
+import com.example.praktikum03.R
+
+object HotelDataList {
+    val HotelDataValue = arrayListOf(
+        HotelData(
+            "Rimba by Ayana Bali",
+            "Terletak di Jimbaran, Rimba by Ayana menawarkan pengalaman menginap yang mewah dengan pemandangan laut yang menakjubkan." +
+                    "Hotel ini memiliki fasilitas lengkap termasuk kolam renang yang luas, spa, dan berbagai pilihan restoran." +
+                    "Ideal untuk liburan romantis maupun keluarga",
+            R.drawable.hotel1,
+            "5.0",
+            "599",
+            "Jimbaran",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "The Apurva Kempinski Bali",
+            "Berada di Nusa Dua, The Apurva Kempinski menonjol dengan desain arsitektur yang megah dan pemandangan laut yang indah. " +
+                    "Hotel ini menyediakan suite dan vila mewah, spa kelas dunia, dan berbagai pilihan restoran gourmet. " +
+                    "Cocok untuk mereka yang mencari kemewahan dan kenyamanan",
+            R.drawable.hotel2,
+            "4.8",
+            "799",
+            "Nusa Dua",
+            R.drawable.location2,
+            "Bali"
+        ),
+        HotelData(
+            "Hard Rock Hotel Bali",
+            "Terletak di Kuta, hotel ini terkenal dengan tema rock and roll-nya. " +
+                    "Hard Rock Hotel Bali menawarkan kolam renang besar dengan berbagai aktivitas hiburan, " +
+                    "fasilitas spa, dan restoran tematik. Tempat ini sangat cocok untuk keluarga dan pecinta musik",
+            R.drawable.hotel3,
+            "4.9",
+            "999",
+            "Kuta",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "Padma Resort Legian",
+            "Padma Resort Legian di Legian menawarkan suasana tropis dengan taman-taman yang rimbun dan kolam renang laguna. " +
+                    "Hotel ini memiliki spa, berbagai pilihan restoran, dan aktivitas untuk anak-anak. " +
+                    "Sangat cocok untuk liburan keluarga",
+            R.drawable.hotel4,
+            "4.7",
+            "899",
+            "Legian",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "The Udaya Resorts & Spa",
+            "Berada di Ubud, The Udaya Resorts & Spa menawarkan ketenangan dengan pemandangan hutan tropis yang hijau. " +
+                    "Hotel ini terkenal dengan layanan spa-nya yang menenangkan, kolam renang pribadi, " +
+                    "dan restoran yang menyajikan makanan lezat. Cocok untuk mereka yang mencari ketenangan dan relaksasi",
+            R.drawable.hotel2,
+            "4.6",
+            "899",
+            "Ubud",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "The Legian Seminyak",
+            "The Legian Seminyak adalah hotel mewah yang terletak di Seminyak, " +
+                    "menawarkan vila dengan kolam renang pribadi dan layanan butler. " +
+                    "Hotel ini memiliki desain yang terinspirasi dari seni dan budaya Bali, " +
+                    "serta fasilitas kelas atas termasuk spa dan restoran mewah",
+            R.drawable.hotel4,
+            "4.8",
+            "799",
+            "Seminyak",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "The Mulia Bali",
+            "Terletak di Nusa Dua, The Mulia Bali adalah salah satu hotel terbaik di dunia dengan pemandangan pantai yang menakjubkan. " +
+                    "Hotel ini menawarkan suite dan vila mewah dengan layanan pelayan pribadi, berbagai kolam renang infinity, " +
+                    "dan restoran gourmet. Ideal untuk liburan mewah dan romantis",
+            R.drawable.hotel1,
+            "4.9",
+            "999",
+            "Nusa Dua",
+            R.drawable.location1,
+            "Bali"
+        ),
+        HotelData(
+            "The St. Regis Bali Resort",
+            "Juga di Nusa Dua, The St. Regis Bali Resort menawarkan kemewahan dengan " +
+                    "vila berkolam renang pribadi, spa mewah, dan layanan butler 24 jam. " +
+                    "Terkenal dengan pantai berpasir putih dan kolam laguna besar, " +
+                    "hotel ini cocok untuk liburan romantis dan mewah",
+            R.drawable.hotel3,
+            "4.7",
+            "899",
+            "Nusa Dua",
+            R.drawable.location1,
+            "Bali"
+        )
+    )
+}
