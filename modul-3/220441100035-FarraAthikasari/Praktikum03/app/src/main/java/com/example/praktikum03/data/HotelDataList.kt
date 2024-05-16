49297bea4a9ce111b1cfb6a343db4561eb0f3757
farraathika <farraathika5@gmail.com>
2024-05-16 19:07:18 +0700

    220441100035-FARRA ATHIKASARI-DWI ANGGA VIRGI-MODUL 3

---
+++ modul-3/220441100035-FarraAthikasari/Praktikum03/app/src/main/java/com/example/praktikum03/data/HotelDataList.kt
@@ -0,0 +1,118 @@
+package com.example.praktikum03.data
+
+import com.example.praktikum03.R
+
+object HotelDataList {
+    val HotelDataValue = arrayListOf(
+        HotelData(
+            "Ubud's Hotel",
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every " +
+                    "individual. One practical step that can be taken is to understand the impact of every daily action on the " +
+                    "environment and look for ways to reduce our ecological footprint. This can start from small decisions " +
+                    "such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste. " +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern. This involves " +
+                    "choosing products that are durable and recyclable, and avoiding waste. Understanding the life cycle " +
+                    "of products, from production to disposal, helps us make more conscious and sustainable choices. " +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally " +
+                    "friendly production and distribution practices can also have a big impact.",
+            R.drawable.hotel1,
+            "5.0",
+            "599",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Sunset Resort & Spa",
+            "Sunset Resort & Spa is the ideal place to unwind and pamper yourself." +
+                    "Surrounded by stunning sunset views, the resort offers a luxurious spa," +
+                    "swimming pool, and other recreational facilities.",
+            R.drawable.hotel2,
+            "4.8",
+            "799",
+            "Nasional Park Yosemite",
+            R.drawable.location2,
+            "California"
+        ),
+        HotelData(
+            "Royal Palace Hotel",
+            "The Royal Palace Hotel offers a luxurious stay in the heart of the city. With its elegant" +
+                    "architecture and opulent interiors, this hotel is perfect for guests seeking a regal " +
+                    "experience. The rooms are spacious and well-appointed, offering modern amenities" +
+                    "and stunning views of the city skyline. ",
+            R.drawable.hotel3,
+            "4.9",
+            "999",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Sunset Beach Resort",
+            "Nestled along the picturesque coastline, Sunset Beach Resort is a haven for beach " +
+                    "lovers. The resort offers comfortable accommodations with private balconies" +
+                    "overlooking the ocean. Guests can relax on the sandy beach, take a dip in the crystal- " +
+                    "clear waters, or enjoy water sports such as snorkeling and kayaking. The resort also" +
+                    "boasts a beachfront restaurant serving delicious seafood and tropical cocktails.",
+            R.drawable.hotel4,
+            "4.7",
+            "899",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Mountain View Lodge",
+            "Situated amidst lush greenery and majestic mountains, Mountain View Lodge offers a " +
+                    "serene retreat for nature enthusiasts. The lodge features cozy cabins with rustic decor " +
+                    "and modern amenities. Guests can explore the surrounding wilderness through hiking " +
+                    "trails or simply relax and enjoy the fresh mountain air.",
+            R.drawable.hotel2,
+            "4.6",
+            "899",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Urban Oasis Hotel",
+            "Urban Oasis Hotel provides a contemporary and stylish accommodation option in the " +
+                    "heart of the bustling city. The hotel's sleek design and modern amenities cater to the " +
+                    "needs of both business and leisure travelers. The rooms are spacious and well-" +
+                    "appointed, featuring comfortable furnishings and high-tech gadgets. Guests can dine " +
+                    "at the hotel's trendy restaurant, relax at the rooftop bar with panoramic city views,",
+            R.drawable.hotel4,
+            "4.8",
+            "799",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Paradise Hotel",
+            "Indulge in luxury and sophistication at Royal Paradise Hotel, " +
+                    "where every detail is designed to exceed your expectations. " +
+                    "From elegant accommodations to impeccable service, " +
+                    "we promise you a truly unforgettable stay.",
+            R.drawable.hotel1,
+            "4.9",
+            "999",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        ),
+        HotelData(
+            "Seaside Resort",
+            "Escape to Seaside Haven Resort and immerse yourself in the beauty " +
+                    "of the ocean and lush tropical gardens. Whether you're looking " +
+                    "for relaxation or adventure, our resort offers the perfect " +
+                    "getaway for your next vacation.",
+            R.drawable.hotel3,
+            "4.7",
+            "899",
+            "Nasional Park Yosemite",
+            R.drawable.location1,
+            "California"
+        )
+    )
+}
