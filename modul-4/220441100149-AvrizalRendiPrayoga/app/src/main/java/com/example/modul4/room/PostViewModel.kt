4ecae67805c692fb0927f888fbef962384a648db
avrzll <220441100149@student.trunojoyo.ac.id>
2024-05-24 18:07:20 +0700

    220441100149_Avrizal Rendi Prayoga_Modul 4_Kak Nuri

---
+++ modul-4/220441100149-AvrizalRendiPrayoga/app/src/main/java/com/example/modul4/room/PostViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.modul4.room
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PostViewModel(private val postRepository: PostRepository) : ViewModel() {
+
+    fun insertPost(post: PostDatabase) {
+        postRepository.insertPost(post)
+    }
+
+    fun getAllPost(): LiveData<List<PostDatabase>> {
+        return postRepository.getAllPost()
+    }
+}
