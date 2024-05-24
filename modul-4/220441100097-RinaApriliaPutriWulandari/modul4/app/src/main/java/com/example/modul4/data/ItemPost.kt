91523e749125fc76122cb9b4e2d993c2744b25b9
riliapuw <rinaapril1504@gmail.com>
2024-05-24 21:46:18 +0700

    220441100097-Rina Aprilia Putri Wulandari-Modul4-Kak Nuri

---
+++ modul-4/220441100097-RinaApriliaPutriWulandari/modul4/app/src/main/java/com/example/modul4/data/ItemPost.kt
@@ -0,0 +1,10 @@
+package com.example.modul4.data
+
+data class ItemPost(
+    val nama: String,
+    val deskripsi: String,
+    val suka: Int,
+    val chat: Int,
+    val gambar: Int,
+    val displayimg: Boolean = true
+)
