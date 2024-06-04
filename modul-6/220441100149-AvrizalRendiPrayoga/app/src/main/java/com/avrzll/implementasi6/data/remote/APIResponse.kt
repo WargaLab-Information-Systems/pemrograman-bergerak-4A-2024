4d75872687edd8e3e856c0bb826f18d1e6f5ebdc
avrzll <220441100149@student.trunojoyo.ac.id>
2024-06-04 20:21:49 +0700

    220441100149_Avrizal Rendi Prayoga_Modul 4, 5_Kak Nuri

--- modul-6/220441100149-AvrizalRendiPrayoga/app/src/main/java/com/avrzll/implementasi6/data/remote/APIResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.avrzll.implementasi6.data.remote
-
-data class APIResponse(
-    val error: Boolean,
-    val message: String,
-    val count: Int,
-    val data: List<PlayerData>
-)
--- modul-6/220441100149-AvrizalRendiPrayoga/app/src/main/java/com/avrzll/implementasi6/data/remote/APIService.kt
+++
@@ -1,9 +0,0 @@
-package com.avrzll.implementasi6.data.remote
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface APIService {
-    @GET("tugas")
-    fun getAllPlayer() : Call<APIResponse>
-}
