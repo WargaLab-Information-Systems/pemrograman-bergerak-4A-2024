c90a5e1bcec530560a88987cdbbc99e70be59dee
natasyapurnama <npurnama51@gmail.com>
2024-06-06 19:15:43 +0700

    220441100091-Natasya Chesa Purnama-Modul 6-Kak Nuri

---
+++ modul-6/220441100091-NatasyaChesaPurnama/Mod6/app/src/main/java/com/example/mod6/data/api/APIResponse.kt
@@ -0,0 +1,11 @@
+package com.example.mod6.data.api
+
+import com.example.mod6.data.playerData
+import com.google.gson.annotations.SerializedName
+
+data class APIResponse(
+    @field:SerializedName("error") val error: Boolean,
+    @field:SerializedName("message") val message: String,
+    @field:SerializedName("count") val count: Int,
+    @field:SerializedName("data") val data: List<playerData>
+)
