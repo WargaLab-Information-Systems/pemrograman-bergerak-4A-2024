13b7a88c300731a97ecb4d1f93bd249299d6720b
Rifqyalfareza <rifqyalfareza30@gmail.com>
2024-06-07 17:25:36 +0700

    220441100131-RifqyAlifuddinAlfareza-Modul3-KakNuri

---
+++ modul-3/220441100131-RifqyAlifuddinAlfareza/Modul3Pember/app/src/main/java/com/example/modul3/Data/DestinasiList.kt
@@ -0,0 +1,195 @@
+package com.example.modul3.Data
+
+import com.example.modul3.R
+
+
+object DestinasiList {
+    val LokasiDum = arrayListOf(
+        LocationData(
+            "Ubud",
+            "Hebat",
+            "Bali",
+            R.drawable.ubud
+        ),
+        LocationData(
+            "Jaddih Hill ",
+            "Sejatinya, Bukit Kapur Jaddih adalah penambangan batu kapur putih dan kini menjelma jadi destinasi wisata yang diburu para pencinta foto. Seperti halnya bukit kapur lain di tanah air, pemandangan di sana juga cukup mirip dengan Bukit Rengel di Tuban atau Bukit Sekapuk di Gresik. Saat berada di sana, kita akan disuguhi pemandangan menarik dari bongkahan sampai guratan kapur putih berukuran besar sampai raksasa. Yang membedakan, Bukit Jaddih punya “kolam renang” bernama Gua Potte. Inilah salah satu spot favorit di Bukit Jaddih. Spesialnya, kolam renang ini terbentuk secara alami karena galian tambang kapur putih yang mengeluarkan mata air. Pemerintah Daerah Kabupaten Bangkalan pun kemudian merenovasinya. Keberadaan kolam renang ini tepat di tengah-tengah Bukit Jaddih.\n" +
+                    "\n",
+            "Madura, East Java",
+            R.drawable.img
+        ),
+        LocationData(
+            "Ranu Regulo ",
+            "Ranu Regulo adalah sebuah danau yang terbentuk secara alami dengan warna air biru kehijauan. Luas Ranu Regulo kurang dari satu hektar. Ranu Regulo dikelilingi pebukitan dan pepohonan hijau sehingga tampak cantik pada saat matahari terbit. Suhu rata-rata yaitu sekitar 10 derajat Celsius dan bisa turun hingga minus 4 derajat Celsius saat dini hari. Pengunjung disarankan untuk tidak berenang di Ranu Regulo dikarenakan suhu air terlalu dingin dan supaya air di danau tersebut tidak tercemar.\n" +
+                    "\n" +
+                    "Lokasi Ranu Regulo masih berada di kawasan Taman Nasional Bromo Tengger Semeru (TNBTS). Tepatnya di desa Ranu Pani Kecamatan Senduro Kabupaten Lumajang Provinsi Jawa Timur.",
+            "Lumajang, East Java",
+            R.drawable.img_3
+        ),
+        LocationData(
+            "Kawah Ijen",
+            "The Ijen volcano complex is a group of composite volcanoes located on the border between Banyuwangi Regency and Bondowoso Regency of East Java, Indonesia. It is known for its blue fire, acidic crater lake, and labour-intensive sulfur mining.\n" +
+                    "\n" +
+                    "It is inside an eponymous larger caldera Ijen, which is about 20 kilometres (12 mi) wide. The Gunung Merapi stratovolcano is the highest point of that complex. The name \"Gunung Merapi\" means 'mountain of fire' in the Indonesian language; Mount Merapi in central Java and Marapi in Sumatra have the same etymology.\n" +
+                    "\n" +
+                    "Ijen Geopark stretches across the entire regency which is specifically in the Mount Ijen area, Pulau Merah Beach, and Alas Purwo National Park. It has various geological, biological, and cultural sites. It became part of UNESCO Global Geoparks in 2023.\n" +
+                    "\n",
+            "Banyuwangi, East Java",
+            R.drawable.img_4
+        ),
+        LocationData(
+            "Penida Island",
+            "Nusa Penida (Balinese: ᬦᬸᬲᬧᭂᬦᬶᬤ), romanized: nusapĕnida, lit. 'Penida Island') is an island located near the southeastern Indonesian island of Bali and a district of Klungkung Regency that includes the neighbouring small island of Nusa Lembongan and twelve even smaller islands. The Badung Strait separates the island and Bali. The interior of Nusa Penida is hilly with a maximum altitude of 524 metres. It is drier than the nearby island of Bali. It is one of the major tourist attractions among the three Nusa islands.\n" +
+                    "\n" +
+                    "There are thirteen small islands nearby – Nusa Lembongan, Nusa Ceningan and eleven even smaller – which are included within the administrative district (kecamatan), which has the same name as the main island. It had a population of 45,110 at the 2010 census, covering 202.84 km2,[1] and the official estimate as at mid 2022 was 59,900.[2]",
+            "Klungkung, Bali ",
+            R.drawable.img_5
+        )
+    )
+
+    val HotelDum = arrayListOf(
+        HotelData(
+            "Padma Resort Ubud",
+            5021,
+            4.9,
+            "Padma Resort Ubud merupakan pilihan yang tepat ketika mengunjungi Payangan. Merupakan perpaduan sempurna antara penghematan dan kenyamanan, properti ini menawarkan suasana yang sesuai untuk keluarga dengan fasilitas yang di desain untuk wisatawan seperti Anda.\n" +
+                    "\n" +
+                    "Karena berdekatan dengan Pura Penataran Er Jeruk (2,5 km), para tamu Padma Resort Ubud dapat mengunjungi salah satu lokasi populer di Payangan.\n" +
+                    "\n" +
+                    "Padma Resort Ubud menawarkan para tamu fasilitas beragam seperti tv layar datar, minibar, dan penyejuk udara, dan mudah untuk terhubung dengan internet, karena tersedia wi-fi gratis.\n" +
+                    "\n" +
+                    "Hotel menawarkan resepsionis 24 jam, layanan concierge, dan layanan kamar, sehingga kunjungan anda semakin spesial. Properti ini juga menyediakan kolam renang dan sarapan. Tamu yang menggunakan kendaraan memiliki akses parkir gratis.\n" +
+                    "\n",
+            R.drawable.img_6
+        ),
+        HotelData(
+            "Kayon Jungle Resort",
+            1605,
+            4.9,
+            "Semua orang butuh beristirahat. Untuk wisatawan yang mengunjungi Ubud, The Kayon Jungle Resort merupakan pilihan terbaik untuk beristirahat dan memulihkan diri. Terkenal akan suasana yang mewah dan lokasinya yang dekat dengan restoran dan tempat terkenal, The Kayon Jungle Resort membuat Anda mudah untuk menikmati Ubud yang terbaik.\n" +
+                    "\n" +
+                    "Kamar di The Kayon Jungle Resort memiliki penyejuk udara, kulkas, dan dapur kecil, dan para tamu tetap dapat menggunakan internet dengan tersedianya wi-fi gratis.\n" +
+                    "\n" +
+                    "Anda juga dapat menikmati kolam renang dan sarapan selama menginap di The Kayon Jungle Resort. Butuh tempat parkir? Parkir gratis tersedia di The Kayon Jungle Resort.\n" +
+                    "\n" +
+                    "Ubud memiliki banyak restoran asia. Jadi pada saat Anda disini, jangan lupa mengunjungi tempat populer seperti Akasha Bali, d'Alas warung, dan Kampung Cafe, yang menyajikan hidangan lezat.\n" +
+                    "\n",
+            R.drawable.img_8
+        ),
+        HotelData(
+            "Seminyak Resort",
+            4747,
+            4.9,
+            "Mencari tempat menginap di Seminyak? Coba lihat The Seminyak Beach Resort & Spa, hotel sesuai untuk keluarga yang menyuguhkan daya tarik terbaik Seminyak ke kamar Anda.\n" +
+                    "\n" +
+                    "Anda akan menikmati kenyamanan kamar yang menawarkan tv layar datar, kulkas, dan penyejuk udara, dan anda dapat tetap menggunakan internet pada saat menginap karena The Seminyak Beach Hotel menawarkan wi-fi gratis.\n" +
+                    "\n" +
+                    "Hotel menawarkan layanan kamar dan layanan concierge. Dan, The Seminyak Beach Resort & Spa juga menyediakan kolam renang dan restoran, untuk memulihkan diri dari hari yang sibuk. Untuk tamu yang mengemudi, parkir gratis tersedia.",
+            R.drawable.seminyak
+        ),
+        HotelData(
+            "Mulia Resort",
+            4580,
+            4.9,
+            "Mulia Resort merupakan pilihan yang tepat ketika mengunjungi Nusa Dua. Merupakan perpaduan sempurna antara penghematan dan kenyamanan, properti ini menawarkan suasana yang romantis dengan fasilitas yang di desain untuk wisatawan seperti Anda.\n" +
+                    "\n" +
+                    "Jika ingin melihat Puja Mandala (2,0 km) sewaktu mengunjungi Nusa Dua, Mulia Resort jaraknya berdekatan.\n" +
+                    "\n" +
+                    "Kamar di Mulia Resort memiliki tv layar datar, minibar, dan penyejuk udara, dan para tamu tetap dapat menggunakan internet dengan tersedianya internet gratis.\n" +
+                    "\n" +
+                    "Anda juga dapat menikmati kolam renang dan sarapan selama menginap di Mulia Resort. Butuh tempat parkir? Parkir tersedia di Mulia Resort.",
+            R.drawable.mulia
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        ),
+        HotelData(
+            "Rimba By Ayana Bali",
+            3234,
+            4.9,
+            "RIMBA Jimbaran BALI by AYANA menawarkan layanan kamar, layanan concierge, dan teras atap. Sebagai tamu RIMBA Jimbaran BALI by AYANA, Anda dapat menikmati kolam renang dan sarapan yang tersedia di properti. Para tamu yang mengemudi memiliki akses ke tempat parkir gratis.\n" +
+                    "\n" +
+                    "Pada saat di Jimbaran, coba kunjungi restoran yang dapat dicapai dengan berjalan kaki dari RIMBA Jimbaran BALI by AYANA, seperti Rock Bar, BALI (0,9 km), UNIQUE Rooftop Bar & Restaurant (0,1 km), dan Kisik Bar & Grill di Ayana Resort and Spa (0,9 km).\n" +
+                    "\n" +
+                    "Oh ya, selama berkunjung, jangan lupa untuk melihat galeri seni, seperti Unagi 2 Handicraft, Souvenir & Painting.\n" +
+                    "\n" +
+                    "RIMBA Jimbaran BALI by AYANA tak sabar untuk menyambut Anda di Jimbaran.",
+            R.drawable.rimba
+        )
+    )
+}
---
+++ modul-3/220441100131-RifqyAlifuddinAlfareza/Modul3Pember/app/src/main/java/com/example/modul3/Data/LocationData.kt
@@ -0,0 +1,8 @@
+package com.example.modul3.Data
+
+data class LocationData(
+    val name: String,
+    val description: String,
+    val lokasi: String,
+    val image: Int
+)
